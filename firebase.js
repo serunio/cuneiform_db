94881692a21cc727605a58bd08de9fce3988fdd6
Jakub Haraszkiewicz <jak.haraszkiewicz@gmail.com>
2026-06-16 15:29:23 +0200

    file name change

--- firebase.js
+++ firebase.js
@@ -1,6 +1,7 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./cunei-collector-firebase-adminsdk-fbsvc-5e2fa0323b.json");
+const serviceAccount = require("./cunei-collector-firebase-adminsdk-fbsvc-1010dbebd5.json");
+
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)

82737f7c4af253b38f00395738c8048a112e39d5
Jakub Haraszkiewicz <jak.haraszkiewicz@gmail.com>
2026-06-20 21:18:05 +0200

    me endpoint

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const c = require('../controllers/users.controller');
+const m = require('../controllers/middleware')
 
 router.get('/login', c.login);
+router.get('/me', m.verifyJWT, c.me)
 
 module.exports = router;

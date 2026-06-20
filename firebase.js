686d53f65773670e4c1597e18e378fe9694fe7ba
Jakub Haraszkiewicz <jak.haraszkiewicz@gmail.com>
2026-06-20 20:26:44 +0200

    fix?

--- firebase.js
+++ firebase.js
@@ -1,15 +1,7 @@
 const admin = require("firebase-admin");
 
-// const serviceAccount = require("./cunei-collector-firebase-adminsdk-fbsvc-1010dbebd5.json");
-
-
-// admin.initializeApp({
-//     credential: admin.credential.cert(serviceAccount)
-// });
-console.log(admin);
-console.log(admin.credential);
 admin.initializeApp({
-    credential: admin.credential.cert({
+    credential: admin.cert({
         projectId: process.env.FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
         privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')

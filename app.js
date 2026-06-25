80ee09cb335dc03542dbe78ac0f3bb6e59bb39b5
Jakub Haraszkiewicz <jak.haraszkiewicz@gmail.com>
2026-06-25 17:38:51 +0200

    test

--- app.js
+++ app.js
@@ -11,4 +11,14 @@ app.use('/cunei', require('./routes/cunei.routes'));
 app.use('/users', require('./routes/users.routes'));
 app.use('/submissions', require('./routes/submissions.routes'))
 
+const https = require("https");
+
+https.get(
+  "https://www.googleapis.com/robot/v1/metadata/x509/securetoken@system.gserviceaccount.com",
+  res => {
+    console.log("STATUS:", res.statusCode);
+    res.on("data", d => console.log(d.toString().slice(0, 200)));
+  }
+).on("error", console.error);
+
 module.exports = app;

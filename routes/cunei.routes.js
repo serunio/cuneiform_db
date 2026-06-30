8898dd7e5dcce4452f9e58c6f5bcf4a4d7659ed6
Jakub Haraszkiewicz <jak.haraszkiewicz@gmail.com>
2026-06-30 17:54:54 +0200

    fix

--- routes/cunei.routes.js
+++ routes/cunei.routes.js
@@ -7,9 +7,10 @@ router.get('/for-user', m.verifyJWT, c.getCuneiForUser)
 router.get('/next', m.verifyJWT, c.getNext)
 router.get('/scrap', m.adminMiddleware, c.scrapCunei);
 router.post('/choose/:id', m.verifyJWT, m.verifyAdmin, c.chooseCunei)
+router.get('/transformed', c.getTransformed)
 router.get('/:id', c.getCunei)
 router.get('/:id/next', c.getNextCunei)
 router.get('/:id/previous', c.getPreviousCunei)
-router.get('/transformed', c.getTransformed)
+
 
 module.exports = router;
